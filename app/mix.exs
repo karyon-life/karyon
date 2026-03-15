1062a4738317e9ace1780375b4117ed0e93f5549
Adrian Webb <adrian@webb.sh>
2026-03-15 19:54:41 -0400

    Updates to the application gateway.

--- app/mix.exs
+++ app/mix.exs
@@ -4,7 +4,7 @@ defmodule App.MixProject do
   def project do
     [
       apps_path: ".",
-      apps: [:core, :nervous_system, :sandbox, :rhizome, :sensory],
+      apps: [:core, :nervous_system, :sandbox, :rhizome, :sensory, :dashboard],
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
