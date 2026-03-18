360ee15cb6458bc65851e5c3795110ce1ad4b2da
Adrian Webb <adrian@webb.sh>
2026-03-18 19:07:02 -0400

    Standardizing chapter 1 and 3 tests with other chapter tests.

---
+++ app/test/chapter1_conformance_runner.exs
@@ -0,0 +1,26 @@
+commands = [
+  {"mix",
+   [
+     "test",
+     "--max-cases",
+     "1",
+     "test/core/architecture_conformance_test.exs",
+     "test/core/motor_driver_test.exs",
+     "test/core/state_recovery_test.exs",
+     "test/core/stem_cell_test.exs"
+   ], Path.expand("../core", __DIR__), %{}}
+]
+
+Enum.each(commands, fn {command, args, cd, env} ->
+  {_, exit_code} =
+    System.cmd(command, args,
+      cd: cd,
+      env: env,
+      into: IO.stream(:stdio, :line),
+      stderr_to_stdout: true
+    )
+
+  if exit_code != 0 do
+    Mix.raise("Chapter 1 conformance failed while running #{command} #{Enum.join(args, " ")}")
+  end
+end)
