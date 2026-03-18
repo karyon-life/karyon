d399c4a84f527433892f8568b110a816049d1f18
Adrian Webb <adrian@webb.sh>
2026-03-18 18:52:18 -0400

    Updates to the sensory libraries.

--- app/sensory/lib/sensory.ex
+++ app/sensory/lib/sensory.ex
@@ -12,6 +12,7 @@ defmodule Sensory do
   defdelegate validate_ingestion(spec), to: Perimeter
   defdelegate parse_repository(path, opts \\ []), to: Sensory.Eyes
   defdelegate project_repository(path, opts \\ []), to: Sensory.Eyes
+  defdelegate ingest_repository_baseline(path, opts \\ []), to: Sensory.BaselineDiet, as: :ingest_repository
   defdelegate normalize_event(spec), to: Sensory.Ears
   defdelegate ingest_event(spec, opts \\ []), to: Sensory.Ears
   defdelegate discover_payload(payload, opts \\ []), to: Sensory.Skin
